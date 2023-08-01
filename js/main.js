519e282971021462864786c21234459027792222
Dennis Nguyen <dennis.nguyen9925@gmail.com>
2023-08-01 23:38:28 +0000

    Added function for renderEntry & DOM tree creation of entry.

--- js/main.js
+++ js/main.js
@@ -23,3 +23,43 @@ $entryForm.addEventListener('submit', function (event) {
   $img.setAttribute('src', 'images/placeholder-image-square.jpg');
   $entryForm.reset();
 });
+
+function renderEntry(entry) {
+  const $newListItem = document.createElement('li');
+  $newListItem.setAttribute('class', 'new-list-item');
+
+  const $newRow = document.createElement('div');
+  $newRow.setAttribute('class', 'row');
+
+  const $entryNew = document.createElement('div');
+  $entryNew.setAttribute('class', 'column-half');
+
+  const $imgNew = document.createElement('img');
+  $imgNew.setAttribute('src', data.entries.photoUrl);
+
+  const $newText = document.createElement('div');
+  $newText.setAttribute('class', 'column-half');
+
+  const $newTitle = document.createElement('h3');
+  $newTitle.textContent = data.entries.title;
+
+  const $newNote = document.createElement('p');
+  $newNote.textContent = data.entries.note;
+
+  $newListItem.appendChild($newRow);
+  $newRow.appendChild($entryNew);
+
+  $entryNew.appendChild($imgNew);
+  $entryNew.appendChild($newText);
+
+  $newText.appendChild($newTitle);
+  $newText.appendChild($newNote);
+
+  return $entryNew;
+}
+
+const $uList = document.querySelector('ul');
+
+for (const entry of data.entries) {
+  $uList.appendChild(renderEntry(entry));
+}
